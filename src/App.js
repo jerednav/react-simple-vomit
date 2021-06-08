497bc6c7a16f2b039f005700d5104761920fb584
jerednav <80506833+jerednav@users.noreply.github.com>
2021-06-08 08:40:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+//imports component functionality from react
 import './App.css';
+//imports app styling
 import Header from './Header';
+//imports header component from the header file
 import SectionMain from './SectionMain';
+// imports sectionMain component from the sectionMain file 
 import Aside from './Aside';
+// imports aside component from the aside file
 import Footer from './Footer';
+// imports footer component from the footer file
 
 class App extends Component {
-
+// creates App as a new component
   render() {
+     // Calls the render function
     return (
-      <div className="App">
-        <Header backColor="green" width="50%"></Header>
+      <div className="App"> // a div with class 'App'
+        <Header backColor="green" width="50%"></Header> 
+      // imported header, given props backColor 'green' width '50%'
         <SectionMain></SectionMain>
+      // imported sectionMain will go here
         <Aside></Aside>
+        // imported aside will go here
         <Footer></Footer>
+      // imported footer will go here
       </div>
     );
   }
 }
 
 export default App;
+// export a single value, which is class App 
