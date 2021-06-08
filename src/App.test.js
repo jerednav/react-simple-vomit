9a934b2c5244637dca0c3b52bdac19f170c7c7f6
jerednav <80506833+jerednav@users.noreply.github.com>
2021-06-08 14:54:04 -0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
+// imports react components
 import ReactDOM from 'react-dom';
+// import react components for interacting with DOM
 import App from './App';
 
+// import App class that is exported from App file
+
 it('renders without crashing', () => {
+  // tests if App renders without crashing in a real browser environment
   const div = document.createElement('div');
+   // creating a div to test with
   ReactDOM.render(<App />, div);
+    // runs a test rendering with App and div
 });
